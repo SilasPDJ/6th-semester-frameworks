3a36fe9a6beaddf5ec43298e7f8f8dcd8f0c222c
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-09-28 01:56:05 -0300

    final version

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
-// Selecionar os elementos do DOM
 let precoUnitario = document.querySelector("#precoUnitario");
 let quantidadeProduto = document.querySelector("#quantidadeProduto");
 let calcularButton = document.querySelector("#calcularButton");
-let resultadoDiv = document.querySelector("#resultado");
+let resultadoLista = document.querySelector("#resultadoLista");
 
-// Adicionar um ouvinte de evento para o botão de cálculo
+let resultados = [];
+
 calcularButton.addEventListener("click", function () {
-  // Obter os valores dos campos de entrada
   let preco = parseFloat(precoUnitario.value);
   let quantidade = parseInt(quantidadeProduto.value);
+  const TOTAL = preco * quantidade;
 
-  // Realizar o cálculo
-  let total = preco * quantidade;
+  resultados.push(TOTAL);
 
-  // Exibir o resultado na div
-  resultadoDiv.textContent = `Total: R$ ${total.toFixed(2)}`;
-});
+  // Exibir o resultado na lista
+  let novoResultadoItem = document.createElement("li");
 
-// Adicionar um ouvinte de evento para limpar o resultado
-resultadoDiv.addEventListener("click", function () {
-  resultadoDiv.textContent = "";
+  let botaoRemover = document.createElement("button");
+  botaoRemover.type = "button"
+  botaoRemover.textContent = "❌";
+  botaoRemover.addEventListener("click", function () {
+    // Encontrar o índice do item na lista
+    let index = resultados.indexOf(TOTAL);
+
+    if (index !== -1) {
+      // Remove o item da lista
+      resultados.splice(index, 1);
+      resultadoLista.removeChild(novoResultadoItem);
+    }
+  });
+
+  // copiar o item ao clicar
+  novoResultadoItem.addEventListener("click", function () {
+    // let itemCopiado = novoResultadoItem.textContent.replace("❌", "");
+    navigator.clipboard.writeText(TOTAL)
+  });
+
+  // novoResultadoItem.textContent = `${resultados.length} - Total: R$ ${TOTAL.toFixed(2)}`;
+  novoResultadoItem.innerHTML = `${resultados.length})<br> PU: R$ ${preco.toFixed(2)} <br> UN: ${quantidade} <br> TOTAL: R$ ${TOTAL.toFixed(2)}`;
+  novoResultadoItem.appendChild(botaoRemover);
+
+  resultadoLista.appendChild(novoResultadoItem);
+
+  // Limpar os campos de entrada
+  precoUnitario.value = "";
+  quantidadeProduto.value = "";
 });
